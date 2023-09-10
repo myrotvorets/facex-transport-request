ecfe77b02db05bebb217452d93b8ee96f5fed0c1
Myrotvorets <support@myrotvorets.center>
2023-09-10 07:28:00 +0300

    Refactor to use AbortSignal

--- lib/index.ts
+++ lib/index.ts
@@ -7,15 +7,21 @@ export class TransportRequest implements IRemoteTransport {
     public post(url: URL, body: string, headers: Record<string, string>, timeout: number): Promise<string> {
         return new Promise<string>((resolve, reject) => {
             let responseBody = '';
-            let tid: NodeJS.Timeout | undefined = undefined;
             const request = url.protocol === 'https:' ? httpsRequest : httpRequest;
             headers['Content-Length'] = `${body.length}`;
+
+            const errorHandler = (error: Error): void => {
+                const e = new NetworkError(error.message, { cause: error });
+                e.body = responseBody;
+                reject(e);
+            };
 
             const req = request(
                 url,
                 {
                     method: 'POST',
                     headers,
+                    signal: AbortSignal.timeout(timeout),
                 },
                 (response: IncomingMessage) => {
                     response.on('data', (chunk) => {
@@ -23,11 +29,10 @@ export class TransportRequest implements IRemoteTransport {
                     });
 
                     response.once('end', () => {
-                        clearTimeout(tid);
                         if (response.statusCode !== 200) {
                             const error = new HttpError({
-                                status: response.statusCode || 0,
-                                statusText: response.statusMessage || '',
+                                status: response.statusCode ?? 0,
+                                statusText: response.statusMessage ?? '',
                             });
 
                             error.body = responseBody;
@@ -36,18 +41,13 @@ export class TransportRequest implements IRemoteTransport {
                             resolve(responseBody);
                         }
                     });
+
+                    response.once('error', errorHandler);
                 },
             );
 
-            req.once('error', (error) => {
-                clearTimeout(tid);
-                const e = new NetworkError(error.message);
-                e.stack = error.stack;
-                e.body = responseBody;
-                reject(e);
-            });
+            req.once('error', errorHandler);
 
-            tid = setTimeout((): unknown => req.destroy(new NetworkError('Timeout')), timeout);
             req.write(body);
             req.end();
         });
